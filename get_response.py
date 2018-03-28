a62e14fae0860d9befa4a54050e03c8b2feb7a73
Weihonghao <SCIN@DN0a237954.SUNet>
2018-03-27 18:03:37 -0700

    tste

--- get_response.py
+++ get_response.py
@@ -289,7 +289,7 @@ def get_text_from_db():
 	#---------------------------------------------------------------------------------------------------------------------------------
 	## onboarding bot
 
-	bot_texts[7][config.OPENNING_INDEX] = Reply(bot_id=7, in_group_id=config.OPENNING_INDEX, texts={topics.GENERAL:[["Hi! I\'m Onboarding-Bot!", "I\'m here to introduce you to all my friends!", "We are here to help you with stress.", "Do you want to hear the introduction?"]]}, next_id=[(config.DEFAULT_NO, 1), (config.DEFAULT_OTHERS, 0)])
+	bot_texts[7][config.OPENNING_INDEX] = Reply(bot_id=7, in_group_id=config.OPENNING_INDEX, texts={topics.GENERAL:[["Hi! I\'m Onboarding-Bot!", "I\'m here to introduce you to all my friends!", "We are here to help you with stress.", "Sounds good?"]]}, next_id=2)#next_id=[(config.DEFAULT_NO, 1), (config.DEFAULT_OTHERS, 0)])
 	
 	tmp_text = [["I\'m here to give you a few pointers about how to interact with me and my friends.",  "First, we are only bots. We strive to do our best to understand you, and you will get more from us if you are able to give more than a yes or no answer to our questions. ", 
 				"We bots are also pretty new, which means that we are still learning.", "Right now it\'s important for us that you respond to each question in one message block.", "Feel free to hit return to add multiple paragraphs but only press send once you have expressed what you want to share. It\'s okay if you forget, we might just get a bit confused", 
